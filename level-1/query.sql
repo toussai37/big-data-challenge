ba2d408a0c1baa0eefc4bac5bcacf2657b174140
Alain Toussaint <alain@Alains-MBP.lan1>
2021-11-18 22:11:04 -0500

    final

---
+++ level-1/query.sql
@@ -0,0 +1,15 @@
+SELECT * FROM review_id_table
+LIMIT 10;
+
+SELECT COUNT(*) FROM review_id_table;
+
+SELECT * FROM products
+LIMIT 10;
+
+SELECT COUNT(*) FROM products;
+
+SELECT * FROM customers
+LIMIT 10;
+
+SELECT COUNT(*) FROM customers;
+
